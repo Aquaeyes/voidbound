86521aeb1611d28a4fa45bcf30756b152e471ba9
mrsterner <simon@sterner.dev>
2024-07-24 11:28:00 +0200

    un-hinge the SimpleSpiritCharge class

--- src/main/kotlin/dev/sterner/block/SpiritBinderBlock.kt
+++ src/main/kotlin/dev/sterner/block/SpiritBinderBlock.kt
@@ -3,14 +3,19 @@ package dev.sterner.block
 import dev.sterner.blockentity.SpiritBinderBlockEntity
 import dev.sterner.registry.VoidBoundBlockEntityTypeRegistry
 import net.minecraft.core.BlockPos
+import net.minecraft.util.StringRepresentable
+import net.minecraft.world.item.context.BlockPlaceContext
 import net.minecraft.world.level.BlockGetter
 import net.minecraft.world.level.Level
 import net.minecraft.world.level.block.BaseEntityBlock
+import net.minecraft.world.level.block.Block
 import net.minecraft.world.level.block.RenderShape
 import net.minecraft.world.level.block.entity.BlockEntity
 import net.minecraft.world.level.block.entity.BlockEntityTicker
 import net.minecraft.world.level.block.entity.BlockEntityType
 import net.minecraft.world.level.block.state.BlockState
+import net.minecraft.world.level.block.state.StateDefinition
+import net.minecraft.world.level.block.state.properties.EnumProperty
 import net.minecraft.world.phys.shapes.BooleanOp
 import net.minecraft.world.phys.shapes.CollisionContext
 import net.minecraft.world.phys.shapes.Shapes
@@ -20,8 +25,20 @@ class SpiritBinderBlock(properties: Properties) : BaseEntityBlock(
     properties.noOcclusion()
 ) {
 
+    init {
+        registerDefaultState(stateDefinition.any().setValue(MODIFIER, Modifier.NONE))
+    }
+
+    override fun getStateForPlacement(context: BlockPlaceContext): BlockState {
+        return super.getStateForPlacement(context)!!.setValue(MODIFIER, Modifier.NONE)
+    }
+
     override fun newBlockEntity(pos: BlockPos, state: BlockState): BlockEntity? {
         return VoidBoundBlockEntityTypeRegistry.SPIRIT_BINDER.get().create(pos, state)
+    }
+
+    override fun createBlockStateDefinition(builder: StateDefinition.Builder<Block, BlockState>) {
+        builder.add(MODIFIER)
     }
 
     override fun <T : BlockEntity?> getTicker(
@@ -44,7 +61,21 @@ class SpiritBinderBlock(properties: Properties) : BaseEntityBlock(
         return makeShape()
     }
 
+    enum class Modifier(name: String) : StringRepresentable {
+        NONE("none"),
+        BRILLIANT("brilliant"),
+        CTHONIC("cthonic"),
+        HEX_ASH("hex_ash");
+
+        override fun getSerializedName(): String {
+            return this.name
+        }
+    }
+
     companion object {
+
+        val MODIFIER = EnumProperty.create("modifier", Modifier::class.java)
+
         fun makeShape(): VoxelShape {
             var shape = Shapes.empty()
             shape =
