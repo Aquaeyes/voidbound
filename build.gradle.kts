50a3141b192e3981ac12dae580635c9c95532002
mrsterner <simon@sterner.dev>
2024-07-25 10:56:42 +0200

    clear world

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,24 +21,6 @@ sourceSets {
 	}
 }
 
-loom {
-	runs {
-		create("dataGen") {
-			client()
-			name("Data Generation")
-			vmArg("-Dfabric-api.datagen")
-			vmArg("-Dfabric-api.datagen.output-dir=${file("src/generated/resources")}")
-			vmArg("-Dfabric-api.datagen.modid=voidbound")
-			//vmArg("-Dfabric-api.datagen.strict-validation")
-
-			property("porting_lib.datagen.existing_resources", file("src/main/resources").absolutePath)
-			property("malum", file("src/main/resources").absolutePath)
-
-			runDir("build/datagen")
-		}
-	}
-}
-
 repositories {
 	mavenCentral()
 	maven ("https://maven.theillusivec4.top/")
--- src/main/kotlin/dev/sterner/components/VoidBoundWorldComponent.kt
+++ src/main/kotlin/dev/sterner/components/VoidBoundWorldComponent.kt
@@ -1,46 +1,16 @@
 package dev.sterner.components
 
 import dev.onyxstudios.cca.api.v3.component.sync.AutoSyncedComponent
-import dev.sterner.VoidBound
-import dev.sterner.registry.VoidBoundComponentRegistry
-import net.minecraft.commands.Commands
-import net.minecraft.core.BlockPos
-import net.minecraft.core.NonNullList
 import net.minecraft.nbt.CompoundTag
-import net.minecraft.nbt.ListTag
-import net.minecraft.nbt.NbtUtils
-import net.minecraft.world.item.ItemStack
 import net.minecraft.world.level.Level
 
 class VoidBoundWorldComponent(val level: Level) : AutoSyncedComponent {
 
-    private var weepingWellList = mutableListOf<BlockPos>()
-
-    fun addWeepingWell(pos: BlockPos) {
-        weepingWellList.add(pos)
-        VoidBoundComponentRegistry.VOID_BOUND_WORLD_COMPONENT.sync(level)
-
-    }
-
     override fun readFromNbt(tag: CompoundTag) {
-        weepingWellList.clear()
-        val listTag = tag.getList("WellList", 10);
-        for (i in listTag.indices) {
-            val compoundTag = listTag.getCompound(i)
-            val pos = NbtUtils.readBlockPos(compoundTag)
-            weepingWellList.add(pos)
-        }
+
     }
 
     override fun writeToNbt(tag: CompoundTag) {
-        val listTag = ListTag()
-        for (pos in weepingWellList) {
-            val tag = CompoundTag()
-            tag.put("WellPos", NbtUtils.writeBlockPos(pos))
-            listTag.add(tag)
-        }
-        if (listTag.isNotEmpty()) {
-            tag.put("WellList", listTag)
-        }
+
     }
 }
