bcc5c4626bbd6ce55ceb915ef7c3673afb9673e1
mrsterner <simon@sterner.dev>
2024-07-31 15:31:03 +0200

    coolio

--- src/main/kotlin/dev/sterner/registry/VoidBoundCreativeTabRegistry.kt
+++ src/main/kotlin/dev/sterner/registry/VoidBoundCreativeTabRegistry.kt
@@ -4,6 +4,7 @@ import dev.sterner.VoidBound
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup
 import net.minecraft.core.Registry
 import net.minecraft.core.registries.BuiltInRegistries
+import net.minecraft.nbt.CompoundTag
 import net.minecraft.network.chat.Component
 import net.minecraft.resources.ResourceLocation
 import net.minecraft.world.item.CreativeModeTab
@@ -15,14 +16,18 @@ object VoidBoundCreativeTabRegistry {
 
     fun init() {
         GROUP = FabricItemGroup.builder().title(Component.translatable("itemGroup." + VoidBound.modid))
-            .icon { VoidBoundItemRegistry.DIVIDER.get().defaultInstance }
+            .icon {
+                val item = VoidBoundItemRegistry.CALL_OF_THE_VOID.get().defaultInstance
+                val tag = CompoundTag()
+                tag.putBoolean("Glowing", true)
+                item.tag = tag
+                item
+            }
             .displayItems { _: ItemDisplayParameters?, entries: CreativeModeTab.Output ->
+
+                entries.accept(VoidBoundItemRegistry.CALL_OF_THE_VOID.get())
                 entries.accept(VoidBoundItemRegistry.EMPTY_SPIRIT_SHARD.get())
                 entries.accept(VoidBoundItemRegistry.DIVIDER.get())
-                entries.accept(VoidBoundItemRegistry.SPIRIT_BINDER.get())
-                entries.accept(VoidBoundItemRegistry.SPIRIT_STABILIZER.get())
-                entries.accept(VoidBoundItemRegistry.CALL_OF_THE_VOID.get())
-                entries.accept(VoidBoundItemRegistry.SOUL_STEEL_GOLEM.get())
                 entries.accept(VoidBoundItemRegistry.CORE_EMPTY.get())
                 entries.accept(VoidBoundItemRegistry.GOLEM_CORE_GATHER.get())
                 entries.accept(VoidBoundItemRegistry.GOLEM_CORE_GUARD.get())
@@ -31,6 +36,9 @@ object VoidBoundCreativeTabRegistry {
                 entries.accept(VoidBoundItemRegistry.GOLEM_CORE_FILL.get())
                 entries.accept(VoidBoundItemRegistry.GOLEM_CORE_EMPTY.get())
                 entries.accept(VoidBoundItemRegistry.GOLEM_CORE_LUMBER.get())
+                entries.accept(VoidBoundItemRegistry.SOUL_STEEL_GOLEM.get())
+                entries.accept(VoidBoundItemRegistry.SPIRIT_BINDER.get())
+                entries.accept(VoidBoundItemRegistry.SPIRIT_STABILIZER.get())
                 //entries.accept(VoidBoundItemRegistry.DESTABILIZED_SPIRIT_RIFT.get())
 
             }.build()
