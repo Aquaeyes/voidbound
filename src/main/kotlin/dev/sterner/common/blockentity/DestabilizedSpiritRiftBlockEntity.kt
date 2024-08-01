9dd613600d0dbbce53743766507c2567832a7c81
mrsterner <simon@sterner.dev>
2024-08-01 12:29:38 +0200

    destabilized rift logic

--- src/main/kotlin/dev/sterner/common/blockentity/DestabilizedSpiritRiftBlockEntity.kt
+++ src/main/kotlin/dev/sterner/common/blockentity/DestabilizedSpiritRiftBlockEntity.kt
@@ -4,6 +4,7 @@ import dev.sterner.api.SyncedBlockEntity
 import dev.sterner.registry.VoidBoundBlockEntityTypeRegistry
 import dev.sterner.registry.VoidBoundParticleTypeRegistry
 import net.minecraft.core.BlockPos
+import net.minecraft.nbt.CompoundTag
 import net.minecraft.world.level.block.state.BlockState
 import team.lodestar.lodestone.handlers.RenderHandler
 import team.lodestar.lodestone.registry.common.particle.LodestoneParticleRegistry
@@ -26,58 +27,103 @@ class DestabilizedSpiritRiftBlockEntity(pos: BlockPos, state: BlockState?) :
     val y = (worldPosition.y.toFloat() + 0.5f).toDouble()
     val z = (worldPosition.z.toFloat() + 0.5f).toDouble()
 
+    private var destabilized = false
+        set(value) {
+            field = value
+            transformationTicks = 20 * 6
+        }
+    private var transformationTicks = 0
+
+    override fun saveAdditional(tag: CompoundTag) {
+        tag.putBoolean("Destabilized", destabilized)
+        tag.putInt("TransformationTicks", transformationTicks)
+        super.saveAdditional(tag)
+    }
+
+    override fun load(tag: CompoundTag) {
+        destabilized = tag.getBoolean("Destabilized")
+        transformationTicks = tag.getInt("TransformationTicks")
+        super.load(tag)
+    }
+
     fun tick() {
         if (level != null && level!!.isClientSide) {
-            if (level!!.gameTime % 16L == 0L) {
-
-                WorldParticleBuilder.create(VoidBoundParticleTypeRegistry.RIFT_PARTICLE.get())
-                    .setRenderTarget(RenderHandler.LATE_DELAYED_RENDER)
-                    .setColorData(
-                        ColorParticleData.create(firstColor, secondColor).setEasing(Easing.SINE_IN).setCoefficient(0.5f)
-                            .build()
-                    )
-                    .setDiscardFunction(SimpleParticleOptions.ParticleDiscardFunctionType.ENDING_CURVE_INVISIBLE)
-                    .setScaleData(GenericParticleData.create(0.2f, 0.2f, 0.2f).build())
-                    .setTransparencyData(
-                        GenericParticleData.create(0.0f, 1.0f, 0.0f).setEasing(Easing.CIRC_IN_OUT).build()
-                    )
-                    .setSpinData(SpinParticleData.create(0.0f, 0.05f).setEasing(Easing.QUARTIC_IN).build())
-                    .spawn(level, x, y, z)
+            if (transformationTicks <= 0) {
+                if (destabilized) {
+                    tickDestabilized()
+                } else {
+                    tickNormal()
+                }
             }
 
-            if (level!!.gameTime % 16L == 8L) {
-
-                WorldParticleBuilder.create(VoidBoundParticleTypeRegistry.RIFT_PARTICLE.get())
-                    .setRenderTarget(RenderHandler.LATE_DELAYED_RENDER)
-                    .setColorData(
-                        ColorParticleData.create(firstColor, secondColor).setEasing(Easing.SINE_IN).setCoefficient(0.5f)
-                            .build()
-                    )
-                    .setDiscardFunction(SimpleParticleOptions.ParticleDiscardFunctionType.ENDING_CURVE_INVISIBLE)
-                    .setScaleData(GenericParticleData.create(0.25f, 0.25f, 0.45f).build())
-                    .setTransparencyData(
-                        GenericParticleData.create(0.0f, 1.0f, 0.0f).setEasing(Easing.CIRC_IN_OUT).build()
-                    )
-                    .setSpinData(SpinParticleData.create(0.0f, -0.05f).setEasing(Easing.QUARTIC_IN).build())
-                    .spawn(level, x, y, z)
+            if (transformationTicks > 0) {
+                transformationTicks--
+                tickTransformation(transformationTicks)
             }
+        }
+    }
+
+    /**
+     * Run particle effects during transformation between normal and destabilized
+     */
+    private fun tickTransformation(transformationTicks: Int) {
+
+    }
+
+    private fun tickDestabilized() {
+
+    }
+
+    private fun tickNormal() {
+        if (level!!.gameTime % 16L == 0L) {
+
+            WorldParticleBuilder.create(VoidBoundParticleTypeRegistry.RIFT_PARTICLE.get())
+                .setRenderTarget(RenderHandler.LATE_DELAYED_RENDER)
+                .setColorData(
+                    ColorParticleData.create(firstColor, secondColor).setEasing(Easing.SINE_IN).setCoefficient(0.5f)
+                        .build()
+                )
+                .setDiscardFunction(SimpleParticleOptions.ParticleDiscardFunctionType.ENDING_CURVE_INVISIBLE)
+                .setScaleData(GenericParticleData.create(0.2f, 0.2f, 0.2f).build())
+                .setTransparencyData(
+                    GenericParticleData.create(0.0f, 1.0f, 0.0f).setEasing(Easing.CIRC_IN_OUT).build()
+                )
+                .setSpinData(SpinParticleData.create(0.0f, 0.05f).setEasing(Easing.QUARTIC_IN).build())
+                .spawn(level, x, y, z)
+        }
+
+        if (level!!.gameTime % 16L == 8L) {
+
+            WorldParticleBuilder.create(VoidBoundParticleTypeRegistry.RIFT_PARTICLE.get())
+                .setRenderTarget(RenderHandler.LATE_DELAYED_RENDER)
+                .setColorData(
+                    ColorParticleData.create(firstColor, secondColor).setEasing(Easing.SINE_IN).setCoefficient(0.5f)
+                        .build()
+                )
+                .setDiscardFunction(SimpleParticleOptions.ParticleDiscardFunctionType.ENDING_CURVE_INVISIBLE)
+                .setScaleData(GenericParticleData.create(0.25f, 0.25f, 0.45f).build())
+                .setTransparencyData(
+                    GenericParticleData.create(0.0f, 1.0f, 0.0f).setEasing(Easing.CIRC_IN_OUT).build()
+                )
+                .setSpinData(SpinParticleData.create(0.0f, -0.05f).setEasing(Easing.QUARTIC_IN).build())
+                .spawn(level, x, y, z)
+        }
 
 
-            if (level!!.gameTime % 2L == 0L) {
+        if (level!!.gameTime % 2L == 0L) {
 
-                WorldParticleBuilder.create(LodestoneParticleRegistry.TWINKLE_PARTICLE)
-                    .setRenderTarget(RenderHandler.LATE_DELAYED_RENDER)
-                    .setScaleData(GenericParticleData.create(0.2f, 0f).build())
-                    .setTransparencyData(GenericParticleData.create(0.2f, 0.8f).build())
-                    .setColorData(
-                        ColorParticleData.create(firstColor, secondColor).setEasing(Easing.SINE_IN).setCoefficient(0.5f)
-                            .build()
-                    )
-                    .setSpinData(SpinParticleData.create(0f, 0.4f).setEasing(Easing.QUARTIC_IN).build())
-                    .setLifetime(20)
-                    .enableNoClip()
-                    .spawn(level, x, y, z)
-            }
+            WorldParticleBuilder.create(LodestoneParticleRegistry.TWINKLE_PARTICLE)
+                .setRenderTarget(RenderHandler.LATE_DELAYED_RENDER)
+                .setScaleData(GenericParticleData.create(0.2f, 0f).build())
+                .setTransparencyData(GenericParticleData.create(0.2f, 0.8f).build())
+                .setColorData(
+                    ColorParticleData.create(firstColor, secondColor).setEasing(Easing.SINE_IN).setCoefficient(0.5f)
+                        .build()
+                )
+                .setSpinData(SpinParticleData.create(0f, 0.4f).setEasing(Easing.QUARTIC_IN).build())
+                .setLifetime(20)
+                .enableNoClip()
+                .spawn(level, x, y, z)
         }
     }
 }
