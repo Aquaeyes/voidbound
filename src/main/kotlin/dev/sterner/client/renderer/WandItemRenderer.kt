d010685f550ff72b0aefe054aad3a7315b7fc779
mrsterner <simon@sterner.dev>
2024-08-04 11:19:13 +0200

    updated texture and model

--- src/main/kotlin/dev/sterner/client/renderer/WandItemRenderer.kt
+++ src/main/kotlin/dev/sterner/client/renderer/WandItemRenderer.kt
@@ -12,8 +12,13 @@ import net.minecraft.client.Minecraft
 import net.minecraft.client.model.ShieldModel
 import net.minecraft.client.renderer.MultiBufferSource
 import net.minecraft.client.renderer.RenderType
+import net.minecraft.core.BlockPos
 import net.minecraft.world.item.ItemDisplayContext
 import net.minecraft.world.item.ItemStack
+import net.minecraft.world.level.block.Blocks
+import net.minecraft.world.level.block.entity.BlockEntity
+import net.minecraft.world.level.block.entity.BlockEntityType
+import net.minecraft.world.level.block.entity.TheEndPortalBlockEntity
 import team.lodestar.lodestone.registry.client.LodestoneRenderTypeRegistry
 import team.lodestar.lodestone.systems.rendering.VFXBuilders
 
@@ -35,16 +40,19 @@ class WandItemRenderer : DynamicItemRenderer {
         }
         val cubeVertexData = RenderUtils.makeCubePositions(0.25f).applyWobble(0f, 0.35f, 0.01f)
         val builder = VFXBuilders.createWorld().setRenderType(LodestoneRenderTypeRegistry.ADDITIVE_TEXTURE.applyAndCache(Tokens.WARD_BORDER))
+        //val builder2 = VFXBuilders.createWorld().setRenderType(LodestoneRenderTypeRegistry.ADDITIVE_TEXTURE.applyAndCache(Tokens.WARD_BORDER)).setColorRaw(0.9f,0.09f,0.9f)
 
         matrices.pushPose()
 
         matrices.translate(0f,0.75f,0f)
-        RenderUtils.drawCube(matrices, builder, 1f, cubeVertexData)
+        //RenderUtils.drawCube(matrices, builder2, 1f, cubeVertexData)
+        //RenderUtils.drawCube(matrices, builder, 1f, cubeVertexData)
 
         matrices.translate(0.5, 0.65, 0.5)
         matrices.scale(1f, -1f, -1f)
         model?.renderToBuffer(matrices, vertexConsumers.getBuffer(RenderType.entityTranslucent(VoidBound.id("textures/item/hallowed_gold_capped_runewood_wand.png"))), light, overlay, 1f,1f,1f,1f)
-
+        matrices.translate(-0.5, -0.65, -0.5)
+        //RenderUtils.drawCube(matrices, builder, 1f, cubeVertexData)
         matrices.popPose()
     }
 }
