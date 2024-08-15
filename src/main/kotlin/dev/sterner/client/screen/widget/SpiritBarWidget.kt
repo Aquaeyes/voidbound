4f324fa69566717ab7bc2bec2d02edb21855dec8
mrsterner <simon@sterner.dev>
2024-08-15 13:27:29 +0200

    new gui for osmotic enchanter

--- src/main/kotlin/dev/sterner/client/screen/widget/SpiritBarWidget.kt
+++ src/main/kotlin/dev/sterner/client/screen/widget/SpiritBarWidget.kt
@@ -26,7 +26,7 @@ class SpiritBarWidget(screen: OsmoticEnchanterScreen, x: Int, y: Int) : Abstract
         //spirit_type.primaryColor
         if (spirit_type != null) {
             val rgba = unpackIntToRGBA(spirit_type!!.primaryColor.rgb)
-            VoidBoundRenderUtils.blit(guiGraphics, icon, x, y, width, height, width, height, 1 - rgba[0] ,1 - rgba[1], 1 - rgba[2], 1f)
+            VoidBoundRenderUtils.blit(guiGraphics, icon, x, y, width, height, width, height, rgba[0], rgba[1], rgba[2], 1f)
 
             if (mouseX >= x && mouseX < x + width && mouseY >= y && mouseY < y + height) {
                 tooltip = Tooltip.create(Component.translatable(spirit_type!!.identifier))
@@ -35,12 +35,11 @@ class SpiritBarWidget(screen: OsmoticEnchanterScreen, x: Int, y: Int) : Abstract
     }
 
     fun unpackIntToRGBA(color: Int): FloatArray {
-        val r = (color shr 24) and 0xFF  // Extract red component (8 most significant bits)
-        val g = (color shr 16) and 0xFF  // Extract green component
-        val b = (color shr 8) and 0xFF   // Extract blue component
-        val a = color and 0xFF           // Extract alpha component (8 least significant bits)
+        val a = (color shr 24) and 0xFF
+        val r = (color shr 16) and 0xFF
+        val g = (color shr 8) and 0xFF
+        val b = color and 0xFF
 
-        // Convert the components to floats (0.0 to 1.0)
         return floatArrayOf(r / 255f, g / 255f, b / 255f, a / 255f)
     }
 
