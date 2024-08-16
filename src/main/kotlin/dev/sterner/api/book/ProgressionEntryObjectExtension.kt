724f70d741218504ed21cf166d4a30c4f8b31a39
mrsterner <simon@sterner.dev>
2024-08-16 07:53:18 +0200

    recipe

--- src/main/kotlin/dev/sterner/api/book/ProgressionEntryObjectExtension.kt
+++ src/main/kotlin/dev/sterner/api/book/ProgressionEntryObjectExtension.kt
@@ -3,6 +3,7 @@ package dev.sterner.api.book
 import com.sammy.malum.client.screen.codex.objects.progression.ProgressionEntryObject
 import net.minecraft.world.item.ItemStack
 
+@Deprecated("Added to Malum")
 interface ProgressionEntryObjectExtension {
     fun `voidbound$setIcon`(itemStack: ItemStack): ProgressionEntryObject
 }
