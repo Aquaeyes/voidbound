6401885052ccf4e9dfcac722c330aa1959d86ab6
mrsterner <simon@sterner.dev>
2024-08-29 22:17:26 +0200

    frikkin end me

--- src/main/kotlin/dev/sterner/common/components/VoidBoundPlayerComponent.kt
+++ src/main/kotlin/dev/sterner/common/components/VoidBoundPlayerComponent.kt
@@ -86,7 +86,7 @@ class VoidBoundPlayerComponent(private val player: Player) : AutoSyncedComponent
 
         fun renderCubeAtPos(ctx: WorldRenderContext) {
             val camera = ctx.camera()
-            val poseStack = ctx.poseStack()
+            val poseStack = ctx.matrixStack()
             val localPlayer = Minecraft.getInstance().player
             if (localPlayer != null) {
                 val playerComponent = VoidBoundComponentRegistry.VOID_BOUND_PLAYER_COMPONENT.get(localPlayer)
