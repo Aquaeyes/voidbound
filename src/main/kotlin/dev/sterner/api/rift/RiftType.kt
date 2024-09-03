556b04c23616142be6ea44119f61471ab0f97e3a
mrsterner <simon@sterner.dev>
2024-09-03 18:12:46 +0200

    fixed server side crash

--- src/main/kotlin/dev/sterner/api/rift/RiftType.kt
+++ src/main/kotlin/dev/sterner/api/rift/RiftType.kt
@@ -27,29 +27,31 @@ abstract class RiftType {
      */
     open fun tick(level: Level, blockPos: BlockPos, blockEntity: SpiritRiftBlockEntity) {
 
-        var transparency = GenericParticleData.create(0.05f, 0.1f).build()
-        var color =
-            ColorParticleData.create(firstColorAlpha, secondColorAlpha).setEasing(Easing.SINE_IN).setCoefficient(0.75f)
-                .build()
-        if (Minecraft.getInstance().player != null) {
-            if (VoidBoundApi.hasGoggles()) {
-                transparency = GenericParticleData.create(0.2f, 0.8f).build()
-                color =
-                    ColorParticleData.create(firstColor, secondColor).setEasing(Easing.SINE_IN).setCoefficient(0.75f)
-                        .build()
+        if (level.isClientSide) {
+            var transparency = GenericParticleData.create(0.05f, 0.1f).build()
+            var color =
+                ColorParticleData.create(firstColorAlpha, secondColorAlpha).setEasing(Easing.SINE_IN).setCoefficient(0.75f)
+                    .build()
+            if (Minecraft.getInstance().player != null) {
+                if (VoidBoundApi.hasGoggles()) {
+                    transparency = GenericParticleData.create(0.2f, 0.8f).build()
+                    color =
+                        ColorParticleData.create(firstColor, secondColor).setEasing(Easing.SINE_IN).setCoefficient(0.75f)
+                            .build()
+                }
             }
-        }
 
-        if (level.gameTime % 2L == 0L) {
-            WorldParticleBuilder.create(LodestoneParticleRegistry.WISP_PARTICLE)
-                .setRenderTarget(RenderHandler.LATE_DELAYED_RENDER)
-                .setScaleData(GenericParticleData.create(0.2f, 0f).build())
-                .setTransparencyData(transparency)
-                .setColorData(color)
-                .setSpinData(SpinParticleData.create(0f, 0.4f).setEasing(Easing.QUARTIC_IN).build())
-                .setLifetime(20)
-                .enableNoClip()
-                .spawn(level, blockPos.x + 0.5, blockPos.y + 0.5, blockPos.z + 0.5)
+            if (level.gameTime % 2L == 0L) {
+                WorldParticleBuilder.create(LodestoneParticleRegistry.WISP_PARTICLE)
+                    .setRenderTarget(RenderHandler.LATE_DELAYED_RENDER)
+                    .setScaleData(GenericParticleData.create(0.2f, 0f).build())
+                    .setTransparencyData(transparency)
+                    .setColorData(color)
+                    .setSpinData(SpinParticleData.create(0f, 0.4f).setEasing(Easing.QUARTIC_IN).build())
+                    .setLifetime(20)
+                    .enableNoClip()
+                    .spawn(level, blockPos.x + 0.5, blockPos.y + 0.5, blockPos.z + 0.5)
+            }
         }
     }
 }
